487f317d9b35d6b214528f4040e0e427d638a395
Xavier Yribarren <xaboule@gmail.com>
2024-01-31 17:14:30 +0100

    switch

--- src/Sound.jsx
+++ src/Sound.jsx
@@ -10,81 +10,23 @@ function Sound({ url, on, paused, context, ...props }) {
 
   const [listener] = useState(() => new THREE.AudioListener());
 
-  const wet = new AudioBufferSourceNode(listener.context)
-  const dede = listener.context.createDelay(.5);
+const audioNonPos = new THREE.Audio()
 
-// wet.setBuffer(buffer)
+audioNonPos.buffer = buffer
 
-// console.log(wet)
-const wetGain = listener.context.createGain()
-// wetGain.connect(listener.context.destination)
-  
-// const s2 = listener.context.createBufferSource()
-wet.buffer = buffer
+audioNonPos.disconnect()
 
-// s2.connect(wet)
-  useEffect(() => {
-    if (on) {
-      wet.start()
-      wet.context.resume()
-      // sound.current.play();
-      // wet.play()
-    } else if (!on) {
-      // sound.current.pause();
-      // wetGain.gain.value = 0
-      wet.context.suspend()
-      
-    }
+const delay = audioNonPos.context.createDelay(.5)
 
-    if (paused) {
-      sound.current.setVolume(0);
-      
-      // wetGain.gain.value = 0
-      // click(true)
-    } else if (!paused) {
-      // wetGain.gain.value = 1
-      sound.current.setVolume(1);
-      // click(false)
-    }
-  }, [on, paused]);
+audioNonPos.connect(delay)
 
-  const biquadFilter = listener.context.createBiquadFilter();
-
-
-
-  wet.connect(dede)
-
-  useEffect(() => {
-    biquadFilter.type = 'lowshelf';
-    biquadFilter.frequency.setValueAtTime(1000, listener.context.currentTime);
-    biquadFilter.gain.setValueAtTime(props.delayTime * -10, listener.context.currentTime);  
-    
-     dede.delayTime.setValueAtTime(
-      props.delayTime,
-      listener.context.currentTime
-    );
-
-    dede.connect(biquadFilter)
-    biquadFilter.connect(listener.context.destination)
- 
-wet.connect(biquadFilter)
-
-  sound.current.hasPlaybackControl= true
-  sound.current.setNodeSource(wet)
-  // sound.current.play()
-
-
-}, [props.delayTime]);
-// console.log()
 
 useEffect(() => {
+  
+  delay.connect(sound.current.getOutput())
   // sound.current.setBuffer(buffer);
   sound.current.setRefDistance(3);
   // sound.current.setLoop(true);
-wet.start()
-wet.context.suspend()
-wet.loop=true
- wet.hasPlaybackControl = true
 
     camera.add(listener);
     return () => camera.remove(listener);
